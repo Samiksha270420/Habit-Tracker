4aefaddcd7ccb9b6cb6734e847ccd4541558f6b7
Samiksha Patil <samjiksha@gmail.com>
2025-03-13 13:49:38 +0530

    Initial commit

---
+++ force-app/main/default/lwc/habitTracker/habitTracker.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class HabitTracker extends LightningElement {
+    // Hello = "Hellow World !!";
+    // habitName = "Mock Data from Apex !!";
+     habitDate = "Today !!";
+    placeHolder = "We are currenlty under construction !!";
+}
